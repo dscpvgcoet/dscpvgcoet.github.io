e7f882d19934b28c4a2cc856e054b981c7adeafc
Yatish Kelkar <yatish1606@gmail.com>
2020-09-22 17:11:55 +0530

    Team page complete

--- src/components/OurTeam.jsx
+++ src/components/OurTeam.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import Header from './Header'
 import Footer from './Footer'
+import { DSCLeadInformation, teamInformation } from '../teamInformation'
+import team from '../assets/images/team.jpg'
 
 import '../css/our-team.css'
 
@@ -13,6 +15,43 @@ class OurTeam extends React.Component {
         this.state = {
             headerHeight: 0
         }
+    }
+
+    componentDidMount() {
+        window.scroll({top: 0, left: 0, behavior:'smooth'})
+    }
+
+    renderCoreTeam = (start, end) => {
+        return teamInformation.core.slice(start,end).map(member => {
+            return (
+                <div className="core-member-box">
+                    <div className="core-member-photo">
+                        <img src={member.photo.replace('open?', 'uc?')}/>
+                    </div>
+                    <h3>{member.name}</h3>    
+                    <p>{member.position}</p>
+                </div>
+            )
+        })
+    }
+
+    renderCompleteTeam = (start, end) => {
+        return teamInformation.team.slice(start,end).map(member => {
+            return (
+                <div className="complete-member-box">
+                    {/* <div className="complete-member-photo-box">
+                        <div className="complete-member-photo-box-photo">
+                            <img src={member.photo}/>
+                        </div>
+                        
+                    </div> */}
+                    <div>
+                        <h3>{member.name}</h3>    
+                        <p>{member.position}</p>
+                    </div>
+                </div>
+            )
+        })
     }
 
     render() {
@@ -31,7 +70,29 @@ class OurTeam extends React.Component {
                         <p>Presenting our passionate team for Developer Students Club PVGCOET 2020-2021</p>
                     </div>
                     <div className="right-side">
-nsodf
+                        <img src={team} style={{width: '40%', objectFit:'cover', marginBottom: 0, display: 'none'}}/>
+                    </div>
+                </div>
+                <div className="team-members-core">
+                    <h3 className="team-title">Core Team</h3>
+                    <div className="team-members-core-inner">
+                        {this.renderCoreTeam(0,4)}
+                    </div>
+                    <div className="team-members-core-inner">
+                        {this.renderCoreTeam(4,8)}
+                    </div>
+                    <h3 className="team-title">Complete Team</h3>
+                    <div className="team-members-core-inner">
+                        {this.renderCompleteTeam(0,6)}
+                    </div>
+                    <div className="team-members-core-inner">
+                        {this.renderCompleteTeam(6,12)}
+                    </div>
+                    <div className="team-members-core-inner">
+                        {this.renderCompleteTeam(12,18)}
+                    </div>
+                    <div className="team-members-core-inner">
+                        {this.renderCompleteTeam(18,24)}
                     </div>
                 </div>
                 <Footer/>
---
+++ src/teamInformation.js
@@ -0,0 +1,153 @@
+import DSCLeadPhoto from './assets/images/sunanda_somwase.jpeg'
+
+export const DSCLeadInformation = {
+    name : 'Sunanda Somwase',
+    photo : './assets/images/sunanda_somwase.jpg',
+    position: 'DSC Lead',
+    quote : 'There are risks and costs to action. But they are far less than the long range risks of comfortable inaction.'
+}
+
+export const teamInformation  = {
+
+    core: [
+        {
+            name : 'Sunanda Somwase',
+            photo : DSCLeadPhoto,
+            position: 'DSC Lead',
+        },
+        {
+            name : 'Adwait Bhope',
+            photo : 'https://drive.google.com/open?id=1KUD-U1x-A_mJ9S1v_QWw648oXOyWEaSq',
+            position: 'Technical Head', 
+        },
+        {
+            name : 'Gaurav Khairnar',
+            photo : 'https://drive.google.com/open?id=1MmA4VgBwPDs6cW5vL8y5fgd3y90zVwBj',
+            position: 'Associate Technical Head', 
+        },
+        {
+            name : 'Shivam Rajput',
+            photo : 'https://drive.google.com/open?id=1pu48w9go7a9rI10drLG_S2r07GK0l5lJ',
+            position: 'Management Head', 
+        },
+        {
+            name : 'Yash Mehta',
+            photo : 'https://drive.google.com/open?id=1pu48w9go7a9rI10drLG_S2r07GK0l5lJ',
+            position: 'Operations Head', 
+        },
+        {
+            name : 'Kshitij Chitnis',
+            photo : 'https://drive.google.com/open?id=19WZVAb4vpxlpBxJiSE1ujT75gRDRt-Wr',
+            position: 'Design and Production Head', 
+        },
+        {
+            name : 'Vinod Kamat',
+            photo : 'https://drive.google.com/open?id=1p9_UEXrEhh1Z0g2RJQ7ahmHAYpfuQTaU',
+            position: 'Project Management Head', 
+        },
+        {
+            name : 'Atharva Dhekne',
+            photo : 'https://drive.google.com/open?id=17lOr1xK-fQmqANAZawGFZbFV_oQ9FQBU',
+            position: 'Chief Editor', 
+        },
+
+    ],
+    team : [
+        {
+            name : 'Amey Satwe',
+            position: 'Cloud Expert',
+        },
+        {
+            name : 'Yatish Kelkar',
+            position: 'UI UX Expert',
+        },
+        {
+            name : 'Mandar Bhide',
+            position: 'Flutter Expert',
+        },
+        {
+            name : 'Shruti Yadav',
+            position: 'Flutter Expert',
+        },
+        {
+            name : 'Rohit Dalvi',
+            position: 'Web Expert',
+        },
+        {
+            name : 'Tejas Shah',
+            position: 'Web Expert',
+        },
+        {
+            name : 'Shreya Kajbaje',
+            position: 'Android Expert',
+        },
+        {
+            name : 'Prashant Bhandari',
+            position: 'Andoid Expert',
+        },
+        {
+            name : 'Pranav Jojan',
+            position: 'Machine Learning Expert',
+        },
+        {
+            name : 'Rutuja Bhure',
+            position: 'Equity Head',
+        },
+        {
+            name : 'Ajinkya Sahu',
+            position: 'Events Organiser',
+        },
+        {
+            name : 'Adwait Madhekar',
+            position: 'Events Organiser',
+        },
+        {
+            name : 'Prasannaraj Patil',
+            position: 'Logistics Officer',
+        },
+        {
+            name : 'Rasika Deshpande',
+            position: 'Outreach Officer',
+        },
+        {
+            name : 'Sagar Patil',
+            position: 'Design Specialist',
+        },
+        {
+            name : 'Vaibhav Karav',
+            position: 'Social Media Specialist',
+        },
+        {
+            name : 'Akshay Ingle',
+            position: 'Marketing Specialist',
+        },
+        {
+            name : 'Pranav Kulkarni',
+            position: 'Project Manager',
+        },
+        {
+            name : 'Harshada Gundecha',
+            position: 'Project Manager',
+        },
+        {
+            name : 'Mahesh Nahak',
+            position: 'Content Writer',
+        },
+        {
+            name : 'Shreyas Garud',
+            position: 'Content Writer',
+        },
+        {
+            name : '',
+            position: '',
+        },
+        {
+            name : '',
+            position: '',
+        },
+        {
+            name : '',
+            position: '',
+        },
+    ]
+}
