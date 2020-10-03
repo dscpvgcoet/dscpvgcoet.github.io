0315e75e1754829652ebf64a2214f083f7075f20
Adwait Bhope <adwaitbhope@gmail.com>
2020-10-03 19:31:29 +0530

    Updates

---
+++ precache-manifest.26034d2940f075f8d4bb7a28ae1b3846.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e543b5a49b88f51eab87acc6077e243",
+    "url": "/index.html"
+  },
+  {
+    "revision": "781692b352a6c20bf707",
+    "url": "/static/css/main.8f6b096c.chunk.css"
+  },
+  {
+    "revision": "eae0a1437189075b86c9",
+    "url": "/static/js/2.fbc02e30.chunk.js"
+  },
+  {
+    "revision": "9ee1d11e4fb7fb7378917db6bce4eed8",
+    "url": "/static/js/2.fbc02e30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "781692b352a6c20bf707",
+    "url": "/static/js/main.ad195019.chunk.js"
+  },
+  {
+    "revision": "b915b02aa9a50b563bf9",
+    "url": "/static/js/runtime-main.d2e4bddf.js"
+  },
+  {
+    "revision": "95c010f1e523daf259fe1d2607645f47",
+    "url": "/static/media/DSCPVGCOETHorizontal.95c010f1.png"
+  },
+  {
+    "revision": "973fc04b3aa04afe7a4911bfe0eeb78a",
+    "url": "/static/media/DSCPVGCOETVertical.973fc04b.png"
+  },
+  {
+    "revision": "371a5ea2b9b9c08f64abf13ee4965623",
+    "url": "/static/media/gaurav_khairnar.371a5ea2.jpg"
+  },
+  {
+    "revision": "799ad44c7d92b760cf75c2106c8f8e5a",
+    "url": "/static/media/homepage_main.799ad44c.jpg"
+  },
+  {
+    "revision": "7e902205a02291249f6e1f6fc90d3a22",
+    "url": "/static/media/jigsaw.7e902205.png"
+  },
+  {
+    "revision": "9804ab6c5bc1c22b1d22b05d4638fee1",
+    "url": "/static/media/lightbulb.9804ab6c.png"
+  },
+  {
+    "revision": "2b7a22146b2b566d5d5bf3c5caadb15d",
+    "url": "/static/media/sunanda_somwase.2b7a2214.jpeg"
+  },
+  {
+    "revision": "4dd2f5525adf1d4a806d4f6dbacfcc29",
+    "url": "/static/media/team.4dd2f552.jpg"
+  },
+  {
+    "revision": "b6d114870cdfe5e8e7ad39c5429f6607",
+    "url": "/static/media/team.b6d11487.png"
+  }
+]);
