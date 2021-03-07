0c41d8109000c07506a44dbc2a24886755a465b6
Yatish Kelkar <yatish1606@gmail.com>
2021-03-07 21:46:47 +0530

    added content

--- src/components/EventDetails.jsx
+++ src/components/EventDetails.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 
 import '../css/events.css'
 import { Timer } from '../helpers'
+import Footer from './Footer'
 import Header from './Header'
 
 const EventDetails = ({}) => {
@@ -85,12 +86,20 @@ const EventDetails = ({}) => {
                     </div>
 
                     
-                    <div style={{height:1700, backgroundColor:'greenyellow'}}></div>
+                    <div className='event-content'>
+                        <p className='t2 darkGrey'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse metus eros, pulvinar sed velit nec, volutpat luctus lacus. Morbi neque lorem, dictum sed pharetra pretium, pharetra sit amet diam. Pellentesque et bibendum enim, ac laoreet tellus. In sed mollis nunc, in tincidunt ligula. Phasellus justo lorem, rhoncus at maximus non, molestie vestibulum nunc. Sed est erat, convallis nec est vitae, ultrices lobortis ipsum. Nunc consequat sapien at eros rutrum, ac dictum lectus vestibulum. Curabitur ut lacus sed justo rhoncus ullamcorper. Proin turpis turpis, placerat quis tristique et, dapibus sit amet arcu. Aliquam vitae dolor posuere, consequat diam ac, finibus libero. Donec sollicitudin elementum varius. Morbi lorem leo, malesuada molestie metus rutrum, dictum maximus tellus. Sed finibus orci non augue mattis, id imperdiet erat fringilla. Quisque in ultricies dolor. Maecenas nulla leo, vestibulum quis mauris et, posuere iaculis nisi.
+<br/><br/><br/>
+Donec mauris enim, tempor eget convallis ac, ultricies et diam. Praesent auctor magna at est vestibulum tempus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce eleifend mattis sem eu suscipit. Sed gravida sapien sit amet odio porttitor tincidunt. Nullam quis dolor nisl. Nam laoreet, risus id fermentum commodo, dui lectus vulputate tortor, commodo volutpat turpis leo eu risus. Aenean viverra cursus ligula sit amet sodales. Suspendisse fermentum consectetur magna vitae commodo. Nullam vehicula, sem id rutrum sagittis, lorem ligula posuere enim, et posuere diam tellus in eros. Nam ac elit gravida, cursus risus et, accumsan ante. Curabitur eu imperdiet tellus. Cras mollis aliquam justo nec aliquet. Cras ultrices elit sed nisl vestibulum, vitae suscipit nunc dictum.
+<br/><br/><br/>
+In eu dui at diam egestas ullamcorper eget in orci. Maecenas nec vulputate risus. Duis dictum tristique ligula quis tempus. Etiam tempor metus sit amet leo convallis mattis. Quisque feugiat, ligula sit amet condimentum tempor, nisl ex euismod nibh, a egestas lorem leo et augue. Nullam ligula mi, gravida nec nunc suscipit, sollicitudin imperdiet ante. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+                    </div>
                    
 
                 </div>
                 
             </div>
+
+            <Footer/>
 
         </Fragment>
     )
