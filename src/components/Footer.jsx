7ce667650cf1721e253a74a1842e8526fd252440
Rohit Dalvi <rohit.dalvi2001@gmail.com>
2021-03-16 15:12:48 +0530

    Footer component complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,93 +1,148 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import '../css/footer.css'
-import instagramLogo from '../assets/images/icons/instagram.png'
-import twitterLogo from '../assets/images/icons/twitter.png'
-import linkedinLogo from '../assets/images/icons/linkedin.png'
-import mail from '../assets/images/icons/mail.png'
-import phone from '../assets/images/icons/phone.png'
-import location from '../assets/images/icons/pin.png'
+import "../css/footer.css";
+import googleLogo from "../assets/images/icons/google.png";
+import instagramLogo from "../assets/images/icons/instagram.png";
+import twitterLogo from "../assets/images/icons/twitter.png";
+import linkedinLogo from "../assets/images/icons/linkedin.png";
+import youtubeLogo from "../assets/images/icons/youtube.png";
+import mail from "../assets/images/icons/mail.png";
+import phone from "../assets/images/icons/phone.png";
+import location from "../assets/images/icons/pin.png";
+import DSCdarkLogo from "../assets/images/DSC PVGCOET Dark Vertical-Logo.png";
 
 const socialMediaInfo = [
-    {
-        name: 'Instagram',
-        image: instagramLogo,
-        href: 'https://instagram.com/dscpvgcoet',
-    },
-    {
-        name: 'Twitter',
-        image: twitterLogo,
-        href: 'https://twitter.com/dscpvgcoet',
-    },
-    {
-        name: 'LinkedIn',
-        image: linkedinLogo,
-        href: 'https://www.linkedin.com/company/dscpvgcoet',
-    },
-]
+  {
+    image: googleLogo,
+    href:
+      "https://dsc.community.dev/pune-vidyarthi-grihas-college-of-engineering-and-technology-pune/",
+  },
+  {
+    image: instagramLogo,
+    href: "https://instagram.com/dscpvgcoet",
+  },
+  {
+    image: linkedinLogo,
+    href: "https://www.linkedin.com/company/dscpvgcoet",
+  },
+  {
+    image: twitterLogo,
+    href: "https://twitter.com/dscpvgcoet",
+  },
+  {
+    image: youtubeLogo,
+    href: "https://www.youtube.com/channel/UCZ3fJ0kM7K1UBIxSPjF-qrA",
+  },
+];
 
 class Footer extends React.Component {
+  renderSocialMediaIcons = () => {
+    return socialMediaInfo.map((item) => {
+      return (
+        <a href={item.href} target="_blank">
+          <div className="social-media-icons-card">
+            <img src={item.image} className="social-media-icons-image" />
+          </div>
+        </a>
+      );
+    });
+  };
 
-
-    renderSocialMediaIcons = () => {
-        return socialMediaInfo.map(item => {
-            return (
-                <a href={item.href} target="_blank">
-                    <div className="social-media-icons-card">
-                        <img src={item.image} className="social-media-icons-image" />
-                        <p className="social-media-icons-p">{item.name}</p>
-                    </div>
-                </a>
-            )
-        })
-    }
-
-    render() {
-        return (
-            <div className="footer-main">
-                <div className="social-media-icons">
-                    {this.renderSocialMediaIcons()}
-                </div>
-                {/* <div className="footer-text">
+  render() {
+    return (
+      <div className="footer-main">
+        <div className="two-sections">
+          <div className="two-sections-text">
+            <p className="h3 darkText">Get inside the hustle.</p>
+            <p className="h3">Join the community.</p>
+          </div>
+          <div className="social-media-icons">
+            {this.renderSocialMediaIcons()}
+          </div>
+        </div>
+        <div className="h-separator"></div>
+        {/* <div className="footer-text">
                 <h2>We are based in Delhi, India</h2>
                 <p>We work with clients all over the world. Get in touch with us</p>
             </div> */}
 
-                <div className="three-sections">
-                    <div className="section">
-                        <h3>About Us</h3>
-                        <p>Developer Student Clubs PVG's COET, powered by Google Developer Experts, is a non-profit university based community that aims to inspire intelligent minds in the field of technology. </p>
-                    </div>
-                    <div className="separator"></div>
-                    <div className="section">
-                        <h3>Contact Us</h3>
-                        <span>
-                            <img src={mail} />
-                            <p style={{ textAlign: 'left' }}><a className="footer-link" href="mailto:dscpvgcoet@pvgcoet.ac.in" target="_blank">dscpvgcoet@pvgcoet.ac.in</a></p>
-                        </span>
-                        <span>
-                            <img src={phone} />
-                            <p style={{ textAlign: 'left' }}>+91 90213 93816</p>
-                        </span>
-                        <span>
-                            <img src={location} />
-                            <p style={{ textAlign: 'left' }}><a className="footer-link" href="https://goo.gl/maps/8ExcK48y1z6F4ykG6" target="_blank">PVG's COET, Sahakarnagar, Pune</a></p>
-                        </span>
-                    </div>
-                    <div className="separator"></div>
-                    <div className="section">
-                        <h3>Keep in touch</h3>
-                        <p>We'll be updating the space with more fun content soon! Keep an eye out for some exciting stuff coming up your way!</p>
-                        {/* <div className="app-buttons-div">
-                        <button className="app-button primary">Android</button>
-                        <button className="app-button secondary">iOS</button>
-                    </div> */}
-                    </div>
-                </div>
-
-            </div>
-        )
-    }
+        <div className="three-sections">
+          <div className="section footer-logo-box">
+            <img src={DSCdarkLogo} alt="DSC-dark-Logo" />
+          </div>
+          {/* <div className="separator"></div> */}
+          <div className="section explore-links">
+            <h3>Explore</h3>
+            <ul>
+              <li>
+                <Link to="/" className="link">
+                  Home
+                </Link>
+              </li>
+              {/* <li><Link to="/blogs" className="link">Blogs</Link></li> */}
+              <li>
+                <Link to="/team" className="link">
+                  Team
+                </Link>
+              </li>
+              <li>
+                <Link to="/events" className="link">
+                  Events
+                </Link>
+              </li>
+              <li>
+                <Link to="/" className="link">
+                  Projects
+                </Link>
+              </li>
+              <li>
+                <Link to="/blogs" className="link">
+                  Blogs
+                </Link>
+              </li>
+              <li>
+                <Link to="/" className="link">
+                  Archives
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="section section-icons">
+            <h3>Collaborate with us</h3>
+            <span>
+              <img src={mail} />
+              <p style={{ textAlign: "left" }}>
+                <a
+                  className="footer-link"
+                  href="mailto:dscpvgcoet@pvgcoet.ac.in"
+                  target="_blank"
+                >
+                  dscpvgcoet@pvgcoet.ac.in
+                </a>
+              </p>
+            </span>
+            <span>
+              <img src={phone} />
+              <p style={{ textAlign: "left" }}>+91 90213 93816</p>
+            </span>
+            <span>
+              <img src={location} />
+              <p style={{ textAlign: "left" }}>
+                <a
+                  className="footer-link"
+                  href="https://goo.gl/maps/8ExcK48y1z6F4ykG6"
+                  target="_blank"
+                >
+                  PVG's COET, Sahakarnagar, Pune
+                </a>
+              </p>
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Footer
+export default Footer;
