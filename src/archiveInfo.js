6e89c6bd25ed9116d0268d696df4950df387ea54
SahilDahekar <dahekar30sahil@gmail.com>
2022-08-28 19:29:21 +0530

    Added new archives data

--- src/archiveInfo.js
+++ src/archiveInfo.js
@@ -1,11 +1,34 @@
 const archiveInfo = [
     {
+        year: '2021-22',
+        info: [
+            {
+                title: 'A look at our 21-22 tenure of GDSC',
+                description: 'Take a look over some of the events and fun activites we conducted during 21-22 year',
+                link: 'https://drive.google.com/file/d/1noV0ueuh-15fM8MkdCCDu9YnKAsB40B5/view?usp=sharing',
+                same_page: false
+            },
+        ]
+    },
+    {
+        year: '2021-22',
+        info: [
+            {
+                title: 'A look at our 21-22 GDSC Team',
+                description: 'Take a look at over GDSC Team of the year 21-22',
+                link: '/team-21-22',
+                same_page: true
+            },
+        ]
+    },
+    {
         year: '2020-21',
         info: [
             {
                 title: 'A look back at our first tenure of GDSC',
                 description: 'Take a look over some of the events and fun activites we conducted in our rookie year',
-                link: 'https://drive.google.com/file/d/11ewrjZQwQ_ccEaVCAamnxt0W1Xq99Gb9/view?usp=sharing'
+                link: 'https://drive.google.com/file/d/11ewrjZQwQ_ccEaVCAamnxt0W1Xq99Gb9/view?usp=sharing',
+                same_page: false
             },
         ]
     },
